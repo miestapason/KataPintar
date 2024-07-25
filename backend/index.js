f8a6f06e12e56094c89ccf44dbd47928ea636009
miestapason <miestapason@gmail.com>
2024-07-25 22:47:24 +0800

    Add socket.io integration

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -8,6 +14,13 @@ app.get('/', (req, res) => {
     res.send('Hello, KataPintar!');
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
